be857ff96621e81fcd78e25cf33133c65b899796
Yunzi Zhang <yunzzha@microsoft.com>
2020-06-28 13:03:42 -0700

    Modify nsg rules of HDB

--- deploy/terraform/modules/hdb_node/hdb-nsg.tf
+++ deploy/terraform/modules/hdb_node/hdb-nsg.tf
@@ -4,35 +4,19 @@
 |                                                                              |
 +--------------------------------------4--------------------------------------*/
 
-# Creates network security rule to allow internal traffic for SAP db subnet
-resource "azurerm_network_security_rule" "nsr-internal-db" {
-  count                       = local.enable_deployment ? (var.infrastructure.vnets.sap.subnet_db.nsg.is_existing ? 0 : 1) : 0
-  name                        = "allow-internal-traffic"
-  resource_group_name         = var.infrastructure.vnets.sap.subnet_db.nsg.is_existing ? data.azurerm_network_security_group.nsg-db[0].resource_group_name : azurerm_network_security_group.nsg-db[0].resource_group_name
-  network_security_group_name = var.infrastructure.vnets.sap.subnet_db.nsg.is_existing ? data.azurerm_network_security_group.nsg-db[0].name : azurerm_network_security_group.nsg-db[0].name
-  priority                    = 101
-  direction                   = "Inbound"
-  access                      = "allow"
-  protocol                    = "Tcp"
-  source_port_range           = "*"
-  destination_port_range      = "*"
-  source_address_prefix       = var.infrastructure.vnets.sap.address_space
-  destination_address_prefix  = var.infrastructure.vnets.sap.subnet_db.prefix
-}
-
 # Creates network security rule to deny external traffic for SAP db subnet
 resource "azurerm_network_security_rule" "nsr-external-db" {
   count                       = local.enable_deployment ? (var.infrastructure.vnets.sap.subnet_db.nsg.is_existing ? 0 : 1) : 0
   name                        = "deny-inbound-traffic"
   resource_group_name         = var.infrastructure.vnets.sap.subnet_db.nsg.is_existing ? data.azurerm_network_security_group.nsg-db[0].resource_group_name : azurerm_network_security_group.nsg-db[0].resource_group_name
   network_security_group_name = var.infrastructure.vnets.sap.subnet_db.nsg.is_existing ? data.azurerm_network_security_group.nsg-db[0].name : azurerm_network_security_group.nsg-db[0].name
-  priority                    = 102
+  priority                    = 100
   direction                   = "Inbound"
   access                      = "deny"
   protocol                    = "Tcp"
   source_port_range           = "*"
   destination_port_range      = "*"
-  source_address_prefix       = "*"
+  source_address_prefix       = "Internet"
   destination_address_prefix  = var.infrastructure.vnets.sap.subnet_db.prefix
 }
 
@@ -42,7 +26,7 @@ resource "azurerm_network_security_rule" "nsr-admin" {
   name                        = "nsr-subnet-admin"
   resource_group_name         = var.infrastructure.vnets.sap.subnet_admin.nsg.is_existing ? data.azurerm_network_security_group.nsg-admin[0].resource_group_name : azurerm_network_security_group.nsg-admin[0].resource_group_name
   network_security_group_name = var.infrastructure.vnets.sap.subnet_admin.nsg.is_existing ? data.azurerm_network_security_group.nsg-admin[0].name : azurerm_network_security_group.nsg-admin[0].name
-  priority                    = 102
+  priority                    = 101
   direction                   = "Inbound"
   access                      = "allow"
   protocol                    = "Tcp"
